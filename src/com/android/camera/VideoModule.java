2552e4e818b86987e14d72ff5b4bb9fb2c88f0c6
likaid <likaid@codeaurora.org>
2015-07-06 14:01:01 +0800

    SnapdragonCamera: Hide the preview cover when preview has started
    The preview cover was shown when onPause, but didn't hide after
    resume. That caused the video preview turned to black screen.
    
    After the camera preview starts, hide the preview cover if it is
    still showing.
    
    Change-Id: I79fdcd45d46d7c083a132e864bbf42649b100ae4
    CRs-Fixed: 865933

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1229,6 +1229,7 @@ public class VideoModule implements CameraModule,
 
     private void onPreviewStarted() {
         mUI.enableShutter(true);
+        mUI.hidePreviewCover();
     }
 
     @Override
