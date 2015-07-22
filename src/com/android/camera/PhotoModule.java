da2c6a545c36be3343382aa775ce8678dbbb1b19
Likai Ding <likaid@codeaurora.org>
2015-07-22 15:18:44 +0800

    SnapdragonCamera: fix crash taking Macro+ from MMS
    For capture intent, preview stops as soon as first jpeg callback
    arrives, but Macro+ has 7 callbacks. Make preview stop after all
    jpeg callbacks.
    
    Change-Id: Id29cc745aa2030edb60bc15da10d9123fa95bfd0

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1192,7 +1192,9 @@ public class PhotoModule
                 return;
             }
             if (mIsImageCaptureIntent) {
-                stopPreview();
+                if (!mRefocus) {
+                    stopPreview();
+                }
             } else if (mSceneMode == CameraUtil.SCENE_MODE_HDR) {
                 mUI.showSwitcher();
                 mUI.setSwipingEnabled(true);
@@ -1380,6 +1382,7 @@ public class PhotoModule
                         }
 
                     } else {
+                        stopPreview();
                         mJpegImageData = jpegData;
                         if (!mQuickCapture) {
                             mUI.showCapturedImageForReview(jpegData, orientation, mMirror);
